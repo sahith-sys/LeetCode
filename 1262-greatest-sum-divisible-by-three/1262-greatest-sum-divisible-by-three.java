11804e9e2131efbf8be35e825cb58dc94482c4fb
Sahith Kumar Kothuru <sahith.22bce8182@vitapstudent.ac.in>
2025-11-23 19:58:32 +0530

    Time: 16 ms (28.57%), Space: 49.3 MB (47.52%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -1,17 +1,60 @@
 class Solution {
     public int maxSumDivThree(int[] nums) {
         Arrays.sort(nums);
+        int n = nums.length;
         int sums = 0;
         for(int num:nums){
             sums+=num;
         }
         int mod = sums%3;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]%3==mod){
-                sums=sums-nums[i];
-                break;
-            }
+        int[] modarr = new int[n];
+        for(int i=0; i<n; i++){
+            modarr[i] = nums[i]%3;
         }
-        return sums;
+        if(mod==1){
+            int case1 = sums; //eliminating two mod 2's
+            int twocount = 0;
+            for(int i=0; i<n; i++){
+                if(modarr[i]==2 && twocount<2){
+                    case1-=nums[i];
+                    twocount+=1;
+                }
+            }
+            if(twocount<2){
+                case1 = -1;
+            }
+            int case2 = 0; //eliminating mod1
+            for(int j=0; j<n; j++){
+                if(modarr[j]==1){
+                    case2 = sums-nums[j];
+                    break;
+                }
+            }
+            return Math.max(case1,case2);
+        }
+        else if(mod==2){
+            int case1 = sums; //eliminating two 1's;
+            int onecount = 0;
+            for(int i=0; i<n; i++){
+                if(modarr[i]==1 && onecount<2){
+                    case1-=nums[i];
+                    onecount+=1;
+                }
+            }
+            if(onecount<2){
+                case1 = -1;
+            }
+            int case2 = 0; //eliminating mod2
+            for(int j=0; j<n; j++){
+                if(modarr[j]==2){
+                    case2 = sums-nums[j];
+                    break;
+                }
+            }
+            return Math.max(case1,case2);
+        }
+        else{
+            return sums;
+        }
     }
 }
